51beceb6fded7f8388712906960ed79c455c0863
Evgeniya Biryulina <jeckilla@gmail.com>
2023-05-23 23:20:40 +0300

    Added attr self

--- pythonProject84/main.py
+++ pythonProject84/main.py
@@ -2,14 +2,14 @@ import psycopg2
 
 class Table():
     def drop_tables(self, name_of_table):
-        with conn.cursor() as cur:
-            cur.execute("""
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                     DROP TABLE name_of_table=%s;
                     """, name_of_table)
 
-    def create_table(conn, name_of_table: str, column1, column2, column3=None, column4=None):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def create_table(self, conn, name_of_table: str, column1, column2, column3=None, column4=None):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                    CREATE TABLE IF NOT EXISTS name_of_table=%s(
                    column1=%s SERIAL PRIMARY KEY,
                    column2=%s VARCHAR(40),
@@ -17,53 +17,53 @@ class Table():
                    column4=%s INTEGER
                    );
                    """, (name_of_table, column1, column2, column3, column4))
-            return cur.fetchall()
+            return self.cur.fetchall()
 
-    def add_client(conn, name, lastname, email, phones_number=None):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def add_client(self, conn, name, lastname, email, phones_number=None):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                 INSERT INTO clients(name, lastname, email)
                 VALUES (%s, %s, %s)
                 RETURNING id, name, lastname, email;     
                 """, (name, lastname, email))
-        return cur.fetchone()
+        return self.cur.fetchone()
 
-    def add_phone(conn, client_id, phone_id, phone_number):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def add_phone(self, conn, client_id, phone_id, phone_number):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                     UPDATE phone_numbers SET phone_number=%s WHERE client_id=%s AND phone_id=%s;
                     """, (client_id, phone_id, phone_number))
-        return cur.fetchone()
+        return self.cur.fetchone()
 
-    def change_client(conn, client_id, first_name=None, surname=None, email=None, phone_number=None):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def change_client(self, conn, client_id, first_name=None, surname=None, email=None, phone_number=None):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                     UPDATE clients SET first_name = %s, surname = %s, 
                     email = %s, phone_number = %s WHERE client_id=%s;
                     """, (client_id, first_name, surname, email, phone_number))
-        return cur.fetchone()
+        return self.cur.fetchone()
 
-    def delete_phone(conn, client_id, phone_number):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def delete_phone(self, conn, client_id, phone_number):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                     DELETE FROM phone_numbers WHERE client_id=%s AND phone_number=%s;
                     """, (client_id,phone_number))
-        return cur.fetchone()
+        return self.cur.fetchone()
 
-    def delete_client(conn, client_id):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def delete_client(self, conn, client_id):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                     DELETE FROM phone_numbers WHERE client_id=%s;
                     DELETE FROM clients WHERE client_id=%s;
                     """, (client_id,))
 
-    def find_client(conn, first_name=None, surname=None, email=None, phone_number=None):
-        with conn.cursor() as cur:
-            cur.execute("""
+    def find_client(self, conn, first_name=None, surname=None, email=None, phone_number=None):
+        with conn.cursor() as self.cur:
+            self.cur.execute("""
                     SELECT DISTINCT first_name, surname FROM clients
                     WHERE first_name=%s OR surname=%s OR email=%s OR phone_number=%s
                     """, (first_name, surname, email, phone_number))
-        return cur.fetchone()
+        return self.cur.fetchone()
 
 
 with psycopg2.connect(dbname="homework", user="postgres", password="lenochka77") as conn:
